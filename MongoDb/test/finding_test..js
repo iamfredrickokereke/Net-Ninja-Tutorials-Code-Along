4bb9a6a1c33f9e0e9c3be3043ba9ebf57081030c
Kelly online <kellyonline2016@gmail.com>
2020-05-01 23:38:50 +0100

    updated find record file

--- MongoDb/test/finding_test..js
+++ MongoDb/test/finding_test..js
@@ -8,16 +8,20 @@ const colors = require('colors')
 
 describe('***saving records***'.yellow, () => {
 
-    it('saves a record to db', (done) => {
+
+    beforeEach( (done) => {
 
         var char = new marioChar({
             name: 'mario'
         })
 
-        char.save()
-            .then(() => {
-                assert(char.isNew === false);
-                done()
-            })
+        char.save().then( () => {
+            done()
+        })
+    });
+
+    it('find a record', (done) => {
+
+       marioChar.findOne({ name : 'mario'})
     })
 })
