5da6698a1fd6021249cc713d211881c9666f6d3b
Kelly online <kellyonline2016@gmail.com>
2020-05-01 16:35:56 +0100

    created a model and schema

---
+++ MongoDb/models/mariochar.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+
+// creating the schema for the model
+
+const mariocharSchema = new Schema;
+
+mariocharSchema({
+    name : String,
+    weight: Number
+})
+
+
+// model
+
+const marioChar = mongoose.model('mariochar', mariocharSchema)
+
+
+//export the 
+module.exports = marioChar;
+
