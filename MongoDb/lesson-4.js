ad97fb781c5c2809c72b759076c6c44778cac542
Kelly online <kellyonline2016@gmail.com>
2020-05-01 17:39:58 +0100

    updated lesson 4 content

--- MongoDb/lesson-4.js
+++ MongoDb/lesson-4.js
@@ -1,1 +1,25 @@
 //Collections and models
+
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+
+// creating the schema for the model
+
+const mariocharSchema = new Schema;
+
+mariocharSchema({
+    name : String,
+    weight: Number
+})
+
+
+// model
+
+const marioChar = mongoose.model('mariochar', mariocharSchema)
+
+
+//export the model
+module.exports = marioChar;
+
