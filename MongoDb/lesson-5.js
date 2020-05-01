455c8f4e3714d947c6c765a8b28faa1a8e745dc7
Kelly online <kellyonline2016@gmail.com>
2020-05-01 17:42:38 +0100

    created lesson 5 content

---
+++ MongoDb/lesson-5.js
@@ -0,0 +1,15 @@
+// introduction to mocha testing
+
+const mocha = require('mocha');
+
+const assert = require('assert');
+
+
+// describe the test
+
+describe('Some demo test', () => {
+
+    it('Adds 2 numbers', () => {
+        assert( 2 + 2 === 4);
+    })
+});
