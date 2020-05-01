60972c96dc59e96ead6bbccc37df6691239303f4
Kelly online <kellyonline2016@gmail.com>
2020-05-01 09:24:23 +0100

    updated lesson 3 content

--- MongoDb/lesson-3.js
+++ MongoDb/lesson-3.js
@@ -1,1 +1,21 @@
 //Connecting to MongoDb
+
+ // import mongoose
+
+ const mongoose = require('mongoose');
+ const colors = require('colors')
+
+
+ //connect to mongodb database
+
+ mongoose.connect('mongodb://localhost/fred');
+
+
+ //create a eventlistener function once connection is open
+
+ mongoose.connection.once('open', () => { 
+     console.log(`Connection has been made now Rejoice!`.rainbow);
+ }).on('error', (error) => {
+     console.log(`oops, something went wrong`.red, error);
+     
+ })
