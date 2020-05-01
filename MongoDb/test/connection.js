3ab11e712cf2e0cdc2adfcb6400576b46bd64f63
Kelly online <kellyonline2016@gmail.com>
2020-05-01 19:03:58 +0100

    Use Es6 promise and before function

--- MongoDb/test/connection.js
+++ MongoDb/test/connection.js
@@ -4,16 +4,29 @@
  const colors = require('colors')
 
 
+ // using Es6 promise
+ mongoose.Promise = global.Promise;
+
  //connect to mongodb database
 
  mongoose.connect('mongodb://localhost/fred');
 
 
+//make connection before anything else
+
+before((done) => {
+
+    
  //create a eventlistener function once connection is open
 
  mongoose.connection.once('open', () => { 
-     console.log(`Connection has been made now Rejoice!`.rainbow);
- }).on('error', (error) => {
-     console.log(`oops, something went wrong`.red, error);
-     
- })
+    console.log(`Connection has been made now Rejoice!`.rainbow);
+
+    done();
+}).on('error', (error) => {
+    console.log(`oops, something went wrong`.red, error);
+    
+})
+})
+
+
