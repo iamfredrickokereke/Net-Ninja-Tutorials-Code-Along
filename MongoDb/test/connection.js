4ff731fd092803f5fd9345449afd62138266da74
Kelly online <kellyonline2016@gmail.com>
2020-05-01 08:37:44 +0100

    imported colors and made connection to mongodb

---
+++ MongoDb/test/connection.js
@@ -0,0 +1,16 @@
+ // import mongoose
+
+ const mongoose = require('mongoose');
+ const colors = require('colors')
+
+
+ //connect to mongodb database
+
+ mongoose.connect('mongodb://localhost/fred');
+
+
+ //create a eventlistener function once connection is open
+
+ mongoose.connection.once('open', () => { 
+     console.log(`Connection has been made now Rejoice!`.rainbow);
+ })
