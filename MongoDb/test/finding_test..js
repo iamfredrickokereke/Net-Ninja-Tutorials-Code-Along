f6f3eb17923fcf1f303a05e923735042e77b2ba3
Kelly online <kellyonline2016@gmail.com>
2020-05-02 07:28:15 +0100

    updated find record file by ID

--- MongoDb/test/finding_test..js
+++ MongoDb/test/finding_test..js
@@ -22,8 +22,19 @@ describe('***saving records***'.yellow, () => {
 
     it('find a record', (done) => {
 
-       marioChar.findOne({ name : 'mario'}).then(function () {
-           done()
+       marioChar.findOne({ name : 'mario'}).then(function (result) {
+
+            assert(result.name === 'mario');
+            done()
        })
     })
+
+    it('[By ID] find a record ', (done) => {
+
+        marioChar.findOne({ _id : char._id}).then(function (result) {
+ 
+             assert(result._id.toString() === char._id.toString());
+             done()
+        })
+     })
 })
