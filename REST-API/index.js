b5b2eec4d79890b2fb69e2470c43f71bc9fe1890
Kelly online <kellyonline2016@gmail.com>
2020-05-03 12:25:27 +0100

    UPDATED routes and exported

--- REST-API/index.js
+++ REST-API/index.js
@@ -1,14 +1,8 @@
 const express = require('express');
 
 const app = express();
-const Router = express.Router();
 
 
-app.get('/', (request, response) => {
-
-    response.send({type : "GET"})
-    console.log("root request");
-    
-} )
+app.use('/api', require('./routes/api'))
 
 app.listen(process.env.PORT  || 8000, () => console.log('server is running...'))
---
+++ REST-API/routes/api.js
@@ -0,0 +1,56 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+//get a list of subjects
+
+
+router.get('/subjects', (request, response) => {
+
+    response.send({type: 'GET'})
+})   
+
+// add a new subject to the category
+
+router.post('/subjects', (request, response) => {
+
+    response.send({type: 'POST'})
+})       
+
+
+// update a subject from the category
+router.put('/subjects/:id', (request, response) => {
+
+    response.send({type: 'PUT'})
+})       
+
+// delete a subject from the category
+
+router.delete('/subjects/:id', (request, response) => {
+
+    response.send({type: 'DELETE'})
+})       
+
+
+
+
+// create and export as a module.
+
+module.exports = router;
+
+
+
+// router = {
+//     'GET': {
+//         '/':(request, response) => {
+//             response.send({type: 'GET'})
+//         }
+//     },
+
+//     'POST': {
+//         '/subjects': (request, response) => {
+//             response.send({type : 'POST'})
+//         }
+//     }
+// }
