2238cfeafa401af14d51286744f4c25a93b65339
Kelly online <kellyonline2016@gmail.com>
2020-05-03 13:19:13 +0100

    added requirements for body parser

--- REST-API/index.js
+++ REST-API/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
 // const routes = require('./routes/api')
 
-
+// use body parser and return json objects
+app.use(bodyParser.json())
 app.use('/api', require('./routes/api'))
 
 app.listen(process.env.PORT  || 8000, () => console.log('server is running...'))
