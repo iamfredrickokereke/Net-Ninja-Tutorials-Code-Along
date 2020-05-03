f9617198e8be635c3772abbf30cee5cfdd248318
Kelly online <kellyonline2016@gmail.com>
2020-05-03 15:04:20 +0100

    connect to schema

--- REST-API/models/subjects.js
+++ REST-API/models/subjects.js
@@ -5,4 +5,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 
+const subjectSchema = new Schema({
+    username: { type: String, unique: true, required: [true, 'username field is required'] },
+    hash: { type: String, required: true },
+    firstName: { type: String, required: [true, 'firstname is required'] },
+    lastName: { type: String, required: [true, 'lastname is required'] },
+    email: { type: String, required: [true, 'email is required'] },
+    // active: { type: Boolean, default: true, select: false },
+    role: { type: String, enum: ["student", "tutor"], default: "student" },
+    createdDate: { type: Date, default: Date.now }
+})
 
+const subject = mongoose.model('subject', subjectSchema);
+
+module.exports = subject;
