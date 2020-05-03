443770a6731a413a58c759d3a3ba008c1de7b67f
Kelly online <kellyonline2016@gmail.com>
2020-05-03 21:31:15 +0100

    clean code

--- REST-API/models/ninjas.js
+++ REST-API/models/ninjas.js
@@ -6,14 +6,7 @@ const Schema = mongoose.Schema;
 
 
 const NinjaSchema = new Schema({
-    // username: { type: String, unique: true, required: [true, 'username field is required'] },
-    // hash: { type: String, required: true },
-    // firstName: { type: String, required: [true, 'firstname is required'] },
-    // lastName: { type: String, required: [true, 'lastname is required'] },
-    // email: { type: String, required: [true, 'email is required'] },
-    // // active: { type: Boolean, default: true, select: false },
-    // role: { type: String, enum: ["student", "tutor"], default: "student" },
-    // createdDate: { type: Date, default: Date.now }
+   
 
     name: {
         type: String,
